668bbb5ad23e6e10c7a086463bd9cbb88d0d6f54
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2021-11-12 13:19:19 +0100

    Fonction JS pour initialiser le jeu

--- app.js
+++ app.js
@@ -2,3 +2,42 @@
 
 var scores, roundScore, activePlayer, gamePlaying;
 
+/**************************/
+/* INITIALISON LE JEU */
+/************************/
+
+//initialisons le jeu. SI l'utilisateur clique sur le bouton "newgame"" ALORS une fonction javascript initialise les scores, les affichages des images, les sons etc.
+
+// Le bouton et évènement associé. Javascript écoute SI un évènement "click sur le bouton newgame" se produit. Pour écouter, nous indiquons une sélection queryselector pour aider javascript à écouter le bon bouton. SI bouton activé, ALORS function init(){}. Quelles instructions allons-nous insérer dans {};
+document.querySelector(".btn-new").addEventListener('click', init);
+
+function init() {
+  
+//notre variable booléenne est vraie car nous lançons le jeu
+  gamePlaying = true;
+// Comment sont initialisées au début du jeu nos valeurs de la variable score global (Jedïforce) et score courant (Padaforce)
+  scores = [0, 0];
+  roundScore = 0;
+// quel va être le joueur activé 0 (padawan1) ou 1 (padawan 2). Padawan 1 jouera en premier !
+  activePlayer = 0;
+
+  // Comment sont afficher des images et animations au début du jeu
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.anim').style.display = 'block';
+
+
+  // réinitialiser l'affichage de nos scores à 0, pour chacun de nos joueurs (les padawans)
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0'; 
+
+  // S'assurer que le style (classe .active du css) du panneau du joueur courant est bien appliqué et que celui de l'autre joueur est bien désactivé
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+  //Lancer notre ambiance sonore. Une sorte de musique d'attente avant le premier lancé. Nous mettrons sur pause lorsque le premier lancé est réalisé.
+  
+  ambiance.play();
+
+};
+
